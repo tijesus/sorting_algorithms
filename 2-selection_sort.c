c304450b94424d34f9170ecc9f0ea51e9c385238
josiah-praise <praisechinonso21@gmail.com>
2024-02-17 08:58:10 +0100

    Gabriel my oga

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sort array of integers in ascending order
+ * @array: array of integers
+ * @size: the size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t j, i, smallest_index;
+	int tmp;
+
+	if (array == NULL || size == 1) 
+		return;
+
+	for (j = 0; j < size; j++)
+	{
+		smallest_index = j;
+		for (i = j + 1; i < size; i++)
+		{	/*update the smallest index*/
+			if (array[i] < array[smallest_index])
+				smallest_index = i;
+		}
+		if (smallest_index != j)
+		{	/*swap*/
+			tmp  = array[smallest_index];
+			array[smallest_index] = array[j];
+			array[j] = tmp;
+			print_array(array, size);
+		}
+	}
+}
