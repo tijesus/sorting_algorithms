adc33932cb1947a76eb6f48f212ef47650197673
tijesus <emmanueltijesu@gmail.com>
2024-02-17 01:04:24 +0100

    insertion sorting algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,18 +8,18 @@
  */
 void swap(listint_t **head, listint_t *node1, listint_t *node2)
 {
-    if (node1->prev != NULL)
-        node1->prev->next = node2;
-    else
-        *head = node2;
+	if (node1->prev != NULL)
+		node1->prev->next = node2;
+	else
+		*head = node2;
 
-    if (node2->next != NULL)
-        node2->next->prev = node1;
+	if (node2->next != NULL)
+		node2->next->prev = node1;
 
-    node1->next = node2->next;
-    node2->prev = node1->prev;
-    node1->prev = node2;
-    node2->next = node1;
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node1->prev = node2;
+	node2->next = node1;
 }
 
 /**
@@ -39,7 +39,7 @@ void insertion_sort_list(listint_t **list)
 		prev = current->prev;
 
 		while (prev != NULL && current->n < prev->n)
-        {
+		{
 			swap(list, prev, current);
 			print_list(*list);
 			prev = current->prev;
