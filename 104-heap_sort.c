5efe03d93f95997c62c543b39917b138e2b76c9f
tijesus <emmanueltijesu@gmail.com>
2024-02-21 00:33:39 +0100

    sorting using shell sort

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,5 +1,48 @@
 #include "sort.h"
 
+
+/**
+ * swap - Swap two values.
+ * @a: The first element to swap.
+ * @b: The second element to swap.
+ */
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * max_heapify - getting the max heap.
+ * @array: An array of integers representing a binary tree.
+ * @size: The size of the array/tree.
+ * @base: The index of the base row of the tree.
+ * @root: The root node of the binary tree.
+ */
+void max_heapify(int *array, size_t size, size_t base, size_t root)
+{
+	size_t left, right, large;
+
+	left = 2 * root + 1;
+	right = 2 * root + 2;
+	large = root;
+
+	if (left < base && array[left] > array[large])
+		large = left;
+	if (right < base && array[right] > array[large])
+		large = right;
+
+	if (large != root)
+	{
+		swap(array + root, array + large);
+		print_array(array, size);
+		max_heapify(array, size, base, large);
+	}
+}
+
 /**
  * heap_sort - sorting using the heap sort algorithm.
  * @array: An array of integers.
@@ -17,7 +60,7 @@ void heap_sort(int *array, size_t size)
 
 	for (i = size - 1; i > 0; i--)
 	{
-		swap_ints(array, array + i);
+		swap(array, array + i);
 		print_array(array, size);
 		max_heapify(array, size, i, 0);
 	}
